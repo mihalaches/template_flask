c2a423338e8e60f66a96c708bb298621231435b6
Sebastian Mihalache <sebastian.mihalache@devnest.ro>
2024-03-14 11:29:26 +0200

    Add better log handler for internal server error

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,31 @@
-from application import app
+from application import app,loggerInstance
 from modules.user import handlers
+from flask import json
+from werkzeug.exceptions import HTTPException
 
-
+_logger = loggerInstance("errors")
 ## USERS
 
 app.add_url_rule("/users/home",'home',handlers.home)
 app.add_url_rule("/users/get/<user_id>","get_user",handlers.get)
 app.add_url_rule("/users/get_all","get_all_users",handlers.get_all)
+app.add_url_rule("/users/add", "add_new_user", handlers.add_user)
+
+
+
+
+###### ERROR HANDLER ###########
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    _logger.exception(e)
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
--- modules/user/handlers.py
+++ modules/user/handlers.py
@@ -1,8 +1,17 @@
-from flask import jsonify
+from flask import jsonify,request
 from models.Users import User
 from application import loggerInstance
 
 _logger = loggerInstance("users")
+
+def check_if_exist(email,name):
+    u1 = User.query.filter_by(name = name).first()
+    if u1:
+        return True
+    u2 = User.query.filter_by(email = email).first()
+    if u2:
+        return True
+    return False
 
 def home():
     _logger.info("HOME")
@@ -19,3 +28,18 @@ def get_all():
     all_users = User.query.all()
     list_users = [user.serialize() for user in all_users]
     return jsonify({"users":list_users})
+
+def add_user():
+    print(x)
+    name = request.args.get('name')
+    email = request.args.get('email')
+    password = request.args.get('password')
+    if not check_if_exist(email,name):
+        user = User()
+        user.name = name
+        user.email = email
+        user.password = password
+        user.save()
+        return jsonify({"user":user.serialize(),"status":"ok"})
+    return jsonify({"data_req":f"Email : {request.args.get('email')} / Name : {request.args.get('name')} / Password : {request.args.get('password')}"})
+
